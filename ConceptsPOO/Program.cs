e7a3f431047928fa8d4a1fe35970d4fa576d19e9
gabriel angel fernandez jara <gabo2084@hotmail.com>
2022-05-07 13:22:35 -0300

    Agregar archivos de proyecto.

---
+++ ConceptsPOO/Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO;
+
+Console.WriteLine("POO Concepts");
+Console.WriteLine("============");
+
+try
+{
+    Console.WriteLine(new Date(2022,5,18));
+    Console.WriteLine(new Date(2024, 2, 29));
+
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message);
+}

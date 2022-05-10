992e72662e3fd461dce362e66691767fec4b8122
gabriel angel fernandez jara <gabo2084@hotmail.com>
2022-05-10 08:29:16 -0300

    Finish Date class

---
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO;
+
+Console.WriteLine("Concepts POO");
+Console.WriteLine("=============");
+
+try
+{
+    Console.WriteLine(new Date(2022,14,18));
+
+}
+catch (Exception error)
+{
+    Console.WriteLine(error.Message); ;
+}
